fc3100019bbc61d87f50387709db1c35d7c2c3f4
RubenSugianto <ruben.sugianto@gmail.com>
2025-03-16 20:44:59 +0700

    State Scheduling & Batching

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,14 @@ function App() {
   const [chosenCount, setChosenCount] = useState(0);
 
   function handleSetCount(newCount) {
+    // State Scheduling
+    // ini dia ga lgsg bs dipake newCountnya
     setChosenCount(newCount);
+    // kalo ini lgsg berubah
+    // setChosenCount((prevChosenCount) => prevChosenCount + 1);
+    // // dua state ini execute app sekali karna ada state batching
+    // // state batching-> 2 state dalam komponen sama gaakan buat 2 kali execute functionnya
+    // console.log(chosenCount); // Wont Work
   }
 
   return (
