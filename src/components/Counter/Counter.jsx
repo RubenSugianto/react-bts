5c897e4161199e87ebfec9d85ddf9dc26e332558
RubenSugianto <ruben.sugianto@gmail.com>
2025-03-16 19:47:45 +0700

    Avoiding Component Function Executions with memo() & Avoiding Component Function Executions with Clever Structuring & Understanding the useCallback() Hook

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, memo, useCallback } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -27,19 +27,31 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
+/*
+Seperti yang kita ketahui, component akan reexecute kalo ada perubahan state
+Misal di parent, ya bakal execute sampe childnya
+Kayak di memo ini kalo kita taro, dia gabakal run childnya ulang kalo gaperlu gitu
+Memo compares prop values (old sama new), kalo dia beda baru di reexecute
+Don't Overuse Memo()
+1. Use it as high up in the component tree as possible
+2. Checking props with memo() costs performance!
+3. Don't use it on components where props will change frequently
+No 1 untuk menghindari karna dia sampe childnya gabakal di reexecute biar ga bug
+No 2 dan 3 -> bakal lakuin pengecekan yang gaperlu
+*/
+const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
   const initialCountIsPrime = isPrime(initialCount);
 
   const [counter, setCounter] = useState(initialCount);
 
-  function handleDecrement() {
+  const handleDecrement = useCallback(function handleDecrement() {
     setCounter((prevCounter) => prevCounter - 1);
-  }
+  }, []);
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(function handleIncrement() {
     setCounter((prevCounter) => prevCounter + 1);
-  }
+  }, []);
 
   return (
     <section className="counter">
@@ -58,4 +70,6 @@ export default function Counter({ initialCount }) {
       </p>
     </section>
   );
-}
+})
+
+export default Counter;
